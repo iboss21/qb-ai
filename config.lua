f7f5fbc741622bb61892d0700b2d7c7d600c907d
Master Mind <48193931+iboss21@users.noreply.github.com>
2023-08-19 01:49:51 -0400

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+-- Configure AI behavior settings here
+Config.NPC = {
+    -- Define AI behavior settings for NPCs
+}
+
+Config.Animals = {
+    -- Define AI behavior settings for animals
+}
