e5693bdc30d795171ce5f3a9e514d14fc5f6f4b6
Master Mind <48193931+iboss21@users.noreply.github.com>
2023-08-19 01:48:32 -0400

    Create fxmanifeest.lua

---
+++ fxmanifeest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'iBoss21'
+description 'QB AI - Realistic AI Behavior for NPCs and Animals'
+version '1.0.0'
+
+client_script 'clieent/client.lua'
+server_script 'server/server.lua'
+shared_script 'config.lua'
+
+lua54 'yes'
